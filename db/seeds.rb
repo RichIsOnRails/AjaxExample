a06f56cbc2a9d65e6fa1f3d08b7fc47d78a03d24
RichIsOnRails <rich@richonrails.com>
2013-10-12 20:16:11 -0400

    Remove active flag.  Oops.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 Product.delete_all
 
 Product.create!([
-  {id: 1, name: "Nintendo Wii U Premium", price: 250, active: true},
-  {id: 2, name: "XBox 360 250GB", price: 250, active: true},
-  {id: 3, name: "Playstation 3 500 GB", price: 239.95, active: true},
-  {id: 4, name: "Nintendo Wii", price: 99.95, active: true},
-  {id: 5, name: "Nintendo 3DS", price: 174.95, active: true}
+  {id: 1, name: "Nintendo Wii U Premium", price: 250},
+  {id: 2, name: "XBox 360 250GB", price: 250},
+  {id: 3, name: "Playstation 3 500 GB", price: 239.95},
+  {id: 4, name: "Nintendo Wii", price: 99.95},
+  {id: 5, name: "Nintendo 3DS", price: 174.95}
 ])
