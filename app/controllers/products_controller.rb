e0d809928527976542e984388aa4dd8aea475995
RichIsOnRails <rich@richonrails.com>
2013-10-12 20:18:29 -0400

    Remove active flag.  Oops.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,6 @@ class ProductsController < ApplicationController
 
 private
   def product_params
-    params.require(:product).permit(:name, :price, :active)
+    params.require(:product).permit(:name, :price)
   end
 end
